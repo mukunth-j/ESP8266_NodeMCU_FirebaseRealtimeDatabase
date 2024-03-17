6260fd220f85da810f2336ee87d52fa74bbeb17b
Mukunth <jmukunth55@gmail.com>
2024-03-17 23:13:03 +0530

    ESP8266 NodeMCU to Firebase Realtime Database Arduino Code

---
+++ ESP8266_NodeMCU_FirebaseRealtimeDatabaseCode.ino
@@ -0,0 +1,30 @@
+#include<Wire.h>
+#include "FirebaseESP8266.h"
+#include<ESP8266WiFi.h>
+
+const int MledPin = 13;
+char FIREBASE_AUTH [] = " "; // Your Firebase Web API Key
+char FIREBASE_HOST [] = " "; // Your Firebase Host URL
+char WIFI_SSID [] = " ";     // Your WIFI SSID
+char WIFI_PASSWORD [] = " "; // Your WIFI Password
+
+FirebaseData MfirebaseData;
+
+void setup(){
+  Serial.begin(115200);
+  pinMode(MledPin,OUTPUT);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+void loop(){
+  digitalWrite(MledPin, HIGH);
+  Serial.println("LED is ON");
+  Firebase.setString(MfirebaseData, "/data", "ON"); //This will create a path as 'data' and save the value on Firebase
+  delay(5000);  
+
+  digitalWrite(MledPin, LOW);
+  Serial.println("LED is OFF");
+  Firebase.setString(MfirebaseData, "/data", "OFF");
+  delay(5000);  
+}
